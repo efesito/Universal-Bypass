a5a6f7e3206af1116150fc43d98119725d39adac
yunv4ontopheheballs <113239202+yunv4ontopheheballs@users.noreply.github.com>
2022-09-16 15:05:18 +0200

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,8 @@
+local DaHood = 2788229376
+local UntitledHood = 9183932460
+
+if game.PlaceId == DaHood then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/yunv4ontopheheballs/Universal-Bypass/main/Games/DaHood.lua"))()
+elseif game.PlaceId == UntitledHood then
+    --loadstring(game:HttpGet(""))()
+end
